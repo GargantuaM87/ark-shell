1dd2e4e75cc6be14367375eb69faad91762f59e1
NkosiDaniel <xxxenouniverse@gmail.com>
2026-01-19 16:36:28 -0500

    Function to allocate necessary memory for reading a line then returning it as an array of characters

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <errno.h>
 #include <stdio.h>
 
+void ash_loop(void);
+char* ash_read_line(void);
+
 int main(int argc, char **argv)
 {
     // Command loop
@@ -27,3 +30,41 @@ void ash_loop(void)
         free(args);
     } while(status);
 } 
+
+#define ASH_RL_BUFSIZE 1024
+char* ash_read_line(void)
+{
+    int bufsize = ASH_RL_BUFSIZE;
+    int position = 0;
+    char* buffer = malloc(sizeof(char) * bufsize); // a kilobyte of allocated memory
+    int c;
+    // exeception handling
+    if(!buffer) {
+        fprintf(stderr, "ash: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        // Read a character from standard input (could be keyboard)
+        c = getchar();
+
+        // If we hit EOF, replace with null character and return
+        if(c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        // Reallocate if we exceed the buffer size
+        if(position >= bufsize) {
+            bufsize += ASH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer) { // if realloc ever returns null or something else happens
+                fprintf(stderr, "ash: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
